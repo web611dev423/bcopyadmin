ac3af1f719332b61231629ba4744033b87954368
felix-dot1212 <felixrhodes1212@proton.me>
2025-04-14 10:27:01 -0400

    revert backend url

--- context/SocketContext.tsx
+++ context/SocketContext.tsx
@@ -16,7 +16,7 @@ export function SocketProvider({ children }: { children: React.ReactNode }) {
   const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const newSocket = io(process.env.BACKEND_API_URL, {
+    const newSocket = io(process.env.NEXT_PUBLIC_BACKEND_API_URL, {
       withCredentials: true,
       transports: ['websocket'], // Force WebSocket transport
       reconnection: true, // Enable reconnection
--- lib/ftpapi.ts
+++ lib/ftpapi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ftpapi = axios.create({
-  baseURL: process.env.BACKEND_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
   headers: {
     'Content-Type': 'multipart/form-data',
   }
